9607db29d3ab51a8b603ca09eecae12c179a17ad
李欣科 <lixink@8531.cn>
2019-08-14 11:37:40 +0800

    覆盖资源

---
+++ app/src/main/assets/js/basic.js
@@ -0,0 +1,50 @@
+function imageBrowse(jsObject,index) {
+    zjxw.imageBrowse(index);
+}
+
+function imageABrowse(jsObject,index) {
+    zjxw.imageABrowse(index);
+}
+
+function applyNightTheme() {
+    var ui_mode_link = document.getElementById("ui_mode_link");
+    ui_mode_link.setAttribute("href", "file:///android_asset/css/night.css");
+}
+
+function applyDayTheme() {
+    var ui_mode_link = document.getElementById("ui_mode_link");
+    ui_mode_link.setAttribute("href", "file:///android_asset/css/day.css");
+}
+ function replaceImage(index,url){
+       var imgs = document.getElementsByTagName("img"),arr=[];
+       for(var i = 0; i < imgs.length; i++) {
+         if (imgs[i].parentNode.tagName !== 'A') {
+            arr.push(imgs[i]);
+         }
+        }
+        if(arr.length > 0){
+            var a = arr[index];
+            a.setAttribute("src",url);
+        }
+  }
+
+ function replaceAImage(index,url){
+       var imgs = document.getElementsByTagName("img"),arr=[];
+       for(var i = 0; i < imgs.length; i++) {
+         if (imgs[i].parentNode.tagName === 'A') {
+            arr.push(imgs[i]);
+         }
+        }
+        if(arr.length > 0){
+            var a = arr[index];
+            a.setAttribute("src",url);
+        }
+  }
+
+ function musicPause(){
+     var audio = document.getElementsByTagName("audio");
+     for(var i = 0; i < audio.length; i++) {
+         var a = document.getElementsByTagName("audio")[i];
+         a.pause();
+     }
+ }
