c81ba2593dc07057cc6994d60b9820561a7fbc73
王震 <lavalike@yeah.net>
2019-11-18 15:17:46 +0800

    易盾一键登录

--- app/src/main/java/cn/com/zjol/ZjolApplication.java
+++ app/src/main/java/cn/com/zjol/ZjolApplication.java
@@ -30,6 +30,7 @@ import cn.com.zjol.biz.core.db.ThemeMode;
 import cn.com.zjol.biz.core.network.DailyNetworkManager;
 import cn.com.zjol.push.Push;
 import cn.com.zjol.push.insight.Insight;
+import cn.com.zjol.quick_login.OneClickLogin;
 import cn.daily.news.analytics.AnalyticsManager;
 import cn.daily.news.update.UpdateManager;
 import zjol.com.cn.news.location.OnLineLocationManager;
@@ -80,6 +81,7 @@ public class ZjolApplication extends MultiDexApplication {
                     GlideMode.setProvincialTraffic(SettingManager.getInstance().isProvincialTraffic());
                     Push.init(mApp);
                     Insight.init(mApp);
+                    OneClickLogin.init(mApp, "ad356081117148588b7c2376333de0a5");
                     //放在所有初始化的最后面，防止其他第三方SDK重写UncaughtExceptionHandler被覆盖
                     initCrashHandler(debuggable);
                 }
